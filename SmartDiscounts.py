8c4ec3a767232dc22e6c6573e3c4178679f85bd9
Blessed Chianumba <62527934+MrFlygerian@users.noreply.github.com>
2020-04-18 00:56:18 +0100

    Update SmartDiscounts.py

--- SmartDiscounts.py
+++ SmartDiscounts.py
@@ -3,18 +3,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.special import expit
 from collections import OrderedDict
-
+from pylab import rcParams
 from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+rcParams['figure.figsize'] = 14, 8
 
 
-#Create Logistic Rgeression model object
-clf = linear_model.LogisticRegression() # Extra  Logistic Regression params: C=1e40, solver='newton-cg'
-
-
-#Create data (this data set is tiny, this is only a training example)
+#Create data for Smart Discounts (helps for plotting purposes)
 data = OrderedDict(
   amount_spent =  [50,  10, 20, 5,  95,  70,  100,  200, 0],
   send_discount = [0,   1,  1,  1,  0,   0,   0,    0,   1])
+
+#Read in gender classification data 
+data_2 = pd.read_csv('heights_weights_genders.csv')
+A = data_2.drop('Gender', axis = 1)
+B = data_2['Gender']
+
+#Split gender classification data
+A_train, A_test, B_train, B_test = train_test_split(A, B, test_size = 0.2, random_state = 42)
+
+#Replacement copy of target outputs for use in plotting
+T = B.copy()
+T = T.replace('Male', 0)
+T = T.replace('Female', 1)
 
 #Create x and Y values for sigmoid function to be plotted for comparison
 x = np.linspace(-7,12,100)
@@ -26,20 +37,31 @@ X = df['amount_spent'].astype('float').values
 X=X.reshape(9,1)
 y = df['send_discount'].astype('float').values
 
-#Plot data with the sigmoid function for comparison
-fig, axs = plt.subplots(2)
+#Plot smart_discounts data, gender classification and sigmoid
+fig, axs = plt.subplots(3)
 axs[0].scatter(X,y)
-axs[1].plot(x,Y)
+axs[1].scatter(A['Weight'],A['Height'], c = T, cmap = 'copper_r')
+axs[2].plot(x,Y)
 plt.show
 
-#Fit model to the signmoid and use it to make a prediction
+#Create Logistic Rgeression model object
+clf = linear_model.LogisticRegression() # Extra  Logistic Regression params: C=1e40, solver='newton-cg'
+
+#Input new parameters
+new_spend = int(input('How much did you spend today (to the nearest £): '))
+height = int(input('How tall are you (to the nearest cm): '))
+weight = int(input('How much do you weigh (to the nearest kilo): '))
+
+#Fit and predict for smart discounts
 fitted_model = clf.fit(X,y)
-new_spend = 90
 prediction_result = clf.predict([[new_spend]])
 
-#Show the predictions
-if prediction_result == 0.0:
-    print (f"a spend of £{new_spend} will get the discount")
+#Fit and predict for gender classification
+fitted_model_2 = clf.fit(A_train,B_train)
+prediction_result_2 = clf.predict([(weight,height)])
+
+#Print predictions and accuracy of gender prediction
+if prediction_result == 1.0:
+    print(f" \nyou get the discount, and we are {100*(round(clf.score(A,B),2))}% you are a {prediction_result_2[0]}")
 else:
-    print(f"a spend of £{new_spend} won't get the discount")
-        
+     print(f" \nyou don't get the discount, and we are {100*(round(clf.score(A_test,B_test),2))}% you are a {prediction_result_2[0]}")
